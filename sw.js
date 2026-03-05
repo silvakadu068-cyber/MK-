cf658cb30b0f3b106a96c92e3d9ef3aea20429e7
silvakadu068-cyber <silvakadu068@gmail.com>
2026-03-05 19:21:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,4 @@
-const CACHE = 'mk-v3';
-const ASSETS = [
-  '/MK/',
-  '/MK/index.html',
-  '/MK/photos.js',
-  '/MK/musica.mp3',
-  '/MK/icon-192.png',
-  '/MK/icon-512.png',
-  '/MK/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => cached);
-    })
-  );
-});
+const CACHE='mk-v4';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
