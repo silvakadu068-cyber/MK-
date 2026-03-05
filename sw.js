52946b746c06dbb888b5c147cc09a12c59e3cbb2
silvakadu068-cyber <silvakadu068@gmail.com>
2026-03-05 19:45:51 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='mk-v4';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
